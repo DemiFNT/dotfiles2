baf29bde97db5d925e5ee5faeb8cb4dda36b93a3
b <example@example.com>
2019-11-02 22:12:59 +0100

    fix

--- temp/dunst/battery_notif.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-percent=$(cat /sys/class/power_supply/BAT0/capacity)
-if [ "$percent" -lt 6 ]; then
-    dunstify -i critical_battery -t 8000 -r 2593 -u critical "     Power Critical, "$percent"% charge remaining"
-fi
